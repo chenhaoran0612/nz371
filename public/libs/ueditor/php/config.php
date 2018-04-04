bbd3136990dcf6aff975cffba23a8311caa1f6c9
chenhaoran <chrloveyou@126.com>
2018-04-04 16:37:38 +0800

    update

--- public/libs/ueditor/php/config.php
+++ public/libs/ueditor/php/config.php
@@ -8,7 +8,7 @@
  */
  
 return array(
-	'upload_type' => 'qiniu',  // [qiniu|local] 设置上传方式 qiniu 上传到七牛云存储 ,local 上传到本地
+	'upload_type' => 'local',  // [qiniu|local] 设置上传方式 qiniu 上传到七牛云存储 ,local 上传到本地
 	/* 本地上传配置信息 */
 	'orderby'     => 'asc',   // [desc|asc] 列出文件排序方式，仅仅在本地上传时候有效
 	'root_path'	  => $_SERVER['DOCUMENT_ROOT'], //本地上传 本地的绝对路径
