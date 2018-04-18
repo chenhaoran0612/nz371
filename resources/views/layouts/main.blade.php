9bf3132926d7aa971f9a036d8dd0c0c2964a1d18
chenhaoran <chrloveyou@126.com>
2018-04-18 09:01:39 +0800

    手机端操作

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>中牟第二高级中学-职业规划系统</title>
